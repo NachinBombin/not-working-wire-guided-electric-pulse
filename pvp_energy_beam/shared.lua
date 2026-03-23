c358bcac740f860f1c64a47c8d37b038fd6243ac
NachinBombin <solidnachox@gmail.com>
2026-03-23 19:49:33 -0300

    Delete pvp_energy_beam directory

--- pvp_energy_beam/shared.lua
+++
@@ -1,6 +0,0 @@
-ENT.Type        = "anim"
-ENT.Base        = "base_entity"
-ENT.PrintName   = "Energy Beam"
-ENT.Spawnable   = false
-ENT.AdminOnly   = false
-ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
